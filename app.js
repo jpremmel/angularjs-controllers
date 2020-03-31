80cbbb8c1ae2ac2fbe92c2c9320b0eb6dbed3d1d
Jacqueline Remmel <jpremmel@gmail.com>
2020-03-31 10:26:20 -0700

    Update to 'controller as' syntax such that angular expressions contain a dot, making it easier to access variables in nested controllers

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //The first parameter of angular.module defines the name of the module (in this case, 'app'). The second parameter is an array that declares the module dependencies that the module uses.
 angular.module('app', []);
 
-//Now register a controller with our angular module. In order to pass data to the view, we need to inject $scope (by specifying it as a parameter) and add data to it. 
-angular.module('app').controller('MainCtrl', function($scope) {
-  $scope.message = 'hello';
+//Now register a controller with our angular module
+angular.module('app').controller('MainCtrl', function() {
+  var self = this;
 
-  $scope.updateMessage = function(message){
-    $scope.message = message;
+  self.message = 'hello';
+
+  self.updateMessage = function(message){
+    self.message = message;
   };
 });
