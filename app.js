ab03c47289bbe3933f83ef400585ef939d3a1cf0
Jacqueline Remmel <jpremmel@gmail.com>
2020-03-31 10:19:36 -0700

    Add ability to update the message

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ angular.module('app', []);
 //Now register a controller with our angular module. In order to pass data to the view, we need to inject $scope (by specifying it as a parameter) and add data to it. 
 angular.module('app').controller('MainCtrl', function($scope) {
   $scope.message = 'hello';
-  
+
+  $scope.updateMessage = function(message){
+    $scope.message = message;
+  };
 });
